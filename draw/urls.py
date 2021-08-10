1d6281c3a84c4e4e5bbb302e2ab6b207a968c28c
joewychen <powcca@berkeley.edu>
2021-08-10 15:08:47 -0700

    milestone3

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('main', views.main, name='main'),
-    path('main/postprofile', views.postprofile, name='postprofile')
+    path('main/', views.main, name='main'),
+    path('main/postprofile', views.postprofile, name='postprofile'),
+    path('main/contest', views.contest, name='contest'),
+    path('main/search', views.search, name='search'),
+    path('main/post', views.post, name='post'),
+    path('main/personal', views.personal, name = 'personal')
 ]
--- draw/views.py
+++ draw/views.py
@@ -9,3 +9,13 @@ def main(request):
 
 def postprofile(request):
     return render(request, "draw/postprofile.html")
+
+def contest(request):
+    return render(request, "draw/contest.html")
+def search(request):
+    return render(request, "draw/search.html")
+def post(request):
+    return render(request, "draw/post.html")
+
+def personal(request):
+    return render(request, "draw/personal.html")
